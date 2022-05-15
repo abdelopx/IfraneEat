bb8d87123251616ef6efb370d179fbbcdcee3a00
unknown <abdelpro1236@gmail.com>
2022-05-15 21:31:45 +0100

    added progress bar and framer motion

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
 import "../styles/globals.css";
 import { Fragment } from "react";
-import Head from 'next/head'
-
+import Head from "next/head";
+import { motion } from "framer-motion";
+import Router from "next/router";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import nProgress from "nprogress";
+import "../styles/nprogress.css";
 
-function MyApp({ Component, pageProps }) {
+
+Router.events.on("routeChangeStart", nProgress.start);
+Router.events.on("routeChangeError", nProgress.done);
+Router.events.on("routeChangeComplete", nProgress.done);
+
+
+function MyApp({ Component, pageProps, router }) {
   return (
     <Fragment>
       <Head>
@@ -13,8 +22,21 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" href="pizza.png" />
       </Head>
       <NavBar />
-      <Component {...pageProps} />
-     
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
     </Fragment>
   );
 }
